e7d10f7a5a8f56dad23c9ee188492bd25718e3e7
Dmitry Ponyatov <dponyatov@gmail.com>
2017-09-24 14:25:00 +0400

    + FVM elements
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- c.c
+++ c.c
@@ -1,2 +1,10 @@
+// FORTH Virtual Machine
+
 #include "h.h"
+
+uint8_t  M[Msz];		// main memory
+uint32_t Ip=0;			// instruction pointer
+ int32_t D[Dsz],Dp=0;	// data stack
+uint32_t R[Rsz],Rp=0;	// return stack
+
 int main() { return 0; }
--- h.h
+++ h.h
@@ -1,10 +1,18 @@
+// FORTH Virtual Machine
+
 #ifndef _H_HPP
 #define _H_HPP
 
+							// FVM configuration
 #define Msz 0x10000
+#define Rsz 0x1000
+#define Dsz 0x100
 
 #include <stdint.h>
 
-extern uint8_t M[Msz];
+extern uint8_t  M[Msz];		// main memory
+extern uint32_t Ip;			// instruction pointer
+extern  int32_t D[Dsz],Dp;	// data stack
+extern uint32_t R[Rsz],Rp;	// return stack
 
 #endif // _H_HPP
